304861d6d833ac1d1cc724ee53c51f6d0f13a6ce
Sameer Kumar <kumar.sam1612@gmail.com>
2026-03-23 21:54:25 +0530

    Add example demonstrating interfaces and their implementation

---
+++ interfaces/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2 * (r.width + r.height)
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println("area =", g.area())
+	fmt.Println("perim =", g.perim())
+}
+
+func detectCircle(g geometry) {
+	if c, ok := g.(circle); ok {
+		fmt.Println("Yes, its a circle with radius", c.radius)
+	}
+
+}
+
+func main() {
+	r1 := rect{width: 3, height: 4}
+	c1 := circle{radius: 5}
+
+	measure(r1)
+	measure(c1)
+
+	detectCircle(r1)
+	detectCircle(c1)
+}
