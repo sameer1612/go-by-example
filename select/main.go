4ff2ce905755960b35f377601fd672f8c51592f2
Sameer Kumar <kumar.sam1612@gmail.com>
2026-03-24 14:11:11 +0530

    Add example demonstrating select statement with goroutines in Go

---
+++ select/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(time.Second)
+		c1 <- "One is done"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "Two is done"
+	}()
+
+	for range 2 {
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
+	}
+}
